6b114d42f4ce432ee126205f421742be28b54b32
DianaG <dianagaona@pursuit.org>
2019-01-08 12:45:53 -0500

    Diana Gaona

---
+++ forms_1/src/form.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Form extends Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <div className="form">It works</div>;
+    )
+  }
+}
+
+export default Form;
